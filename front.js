d4ded76a1c948ddb26a72390356933b1624eea33
nouh <www.mndb1@gmail.com>
2023-12-21 23:08:48 +0300

    delete unnessery files

---
+++ front.js
@@ -0,0 +1,18 @@
+var d;
+fetch("https://gmailreader.onrender.com/read/ramiko963963")
+  .then((response) => response.json())
+  .then((data) => {
+    d = data;
+    return d;
+  })
+  .then((data) => {
+
+        let code = d[0];
+        let pure_code = code.body.substring(
+            code.body.lastIndexOf("جابر :") + 7
+            // code.body.lastIndexOf("\n\n\n\n\n")
+            );
+             console.log(pure_code)
+
+  })
+  .catch((error) => console.error("Error:", error));
